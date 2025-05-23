1c80b64b6a1f09484b6ad3d738f4d0367893a054
mateuszwiniarczyk <mateuszwiniarczyk99@gmail.com>
2025-05-23 16:36:32 +0200

    feat: add shadcn and implement Button component with variants

--- src/views/jobsList/ui/JobsListPage.tsx
+++ src/views/jobsList/ui/JobsListPage.tsx
@@ -1,6 +1,9 @@
+import { Button } from '@/shared';
+
 export const JobsListPage = () => (
-  <div>
+  <div className="p-4">
     <h1>Jobs List</h1>
     <p>This is a sample Jobs List Page.</p>
+    <Button>Click me</Button>
   </div>
 );
